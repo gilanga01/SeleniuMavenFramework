8365fb668fc458fe085fe7438cccada4b4444f62
Gilbert Langat <Gilbert.Langat@isobar.com>
2022-03-15 13:25:08 +0300

    Extent Reoports

---
+++ src/test/java/seleniummaven/ExtentReportsBasicDemo.java
@@ -0,0 +1,79 @@
+package seleniummaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExtentReportsBasicDemo {
+private static WebDriver driver = null;
+	public static void main(String[] args) throws InterruptedException {
+		
+		// path where output is to be printed
+		ExtentSparkReporter html = new ExtentSparkReporter("ExtentReports.html");
+		ExtentReports extent = new ExtentReports();
+		extent.attachReporter(html);
+		
+		ExtentTest test1 = extent.createTest("Google Search Test one ","this is a test to validate google search");
+		WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        //find element demo
+        
+        
+        
+        test1.log(Status.INFO, "Starting Test case");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("http://www.google.com/");
+        test1.pass("Navigated to google.com");
+        
+        driver.findElement(By.name("q")).sendKeys("Selenium"+ Keys.ENTER);
+        test1.pass("Entered text to Searchbox");
+        
+        
+        //driver.findElement(By.name("btnk")).click();
+        //test1.pass("Pressed keyboard enter key");
+        
+        
+        driver.close();
+        driver.quit();
+        test1.pass("Closed the browser");
+        
+        test1.info("Test completed");
+        
+ExtentTest test2 = extent.createTest("Google Search Test two ","this is a test to validate google search");
+	
+        test2.log(Status.INFO, "Starting Test case");
+        driver = new ChromeDriver();
+        driver.get("http://www.google.com/");
+        test2.pass("Navigated to google.com");
+        
+        driver.findElement(By.name("q")).sendKeys("Selenium"+ Keys.ENTER);
+        test2.fail("Entered text to Searchbox");
+        
+        
+        //driver.findElement(By.name("btnk")).click();
+        //test1.pass("Pressed keyboard enter key");
+        
+        
+        driver.close();
+        driver.quit();
+        test1.pass("Closed the browser");
+        
+        test2.info("Test completed");
+        
+        
+        //calling flush writes everything to the log file
+        extent.flush();	
+		
+	}
+	}
+		
+
--- src/test/java/seleniummaven/TestngDemo.java
+++ src/test/java/seleniummaven/TestngDemo.java
@@ -1,10 +1,11 @@
 package seleniummaven;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+//import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -16,35 +17,32 @@ public class TestngDemo {
 
 	static WebDriver driver = null;
 	@BeforeTest
-	public static void setUpTest() throws InterruptedException {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-	}
+	public static void setUpTest(){
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();}
+	
 
 	@Test
-	public  void EnterEmail() throws InterruptedException {
+	public  void EnterEmail(){
 		driver.get("https://www.facebook.com/login.php");
-
 		WebElement unm = driver.findElement(By.id("email"));
 		unm.sendKeys("0723056984");
-	}
-	@Test
-	public  void enterPassword() throws InterruptedException {
 		WebElement pass = driver.findElement(By.id("pass"));
 		pass.sendKeys("33290450");
-		Thread.sleep(2000);
-	}
+		}
+	
 	@Test
 	public  void Submit() {
 		WebElement submit = driver.findElement(By.id("loginbutton"));
 		submit.click();
-	}
+		}
+	
 	
     @AfterTest
-	public void tearDownTest() {
-		System.out.println("Logged in Successfully.......");
-		//driver.close();
-		//driver.quit();
+	public void tearDownTest(){
+		System.out.println("Logged in Successfully================");
+		driver.close();
+	    driver.quit();
 	}
 }
 
